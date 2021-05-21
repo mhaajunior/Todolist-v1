718cc1516247827c5ccadabff763f09065357771
Thithut Warapho <junior29958@gmail.com>
2021-05-21 12:16:31 +0700

    finish todov1

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+const app = express();
+
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+  const day = date.getDate();
+  res.render("list", {listTitle: day, newListItems: items});
+})
+
+app.post("/", function(req, res) {
+  if (req.body.list === "Work") {
+    workItems.push(req.body.newItem);
+    res.redirect("/work");
+  } else {
+    items.push(req.body.newItem);
+    res.redirect("/");
+  }
+})
+
+app.get("/work", function(req, res) {
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+})
+
+app.get("/about", function(req, res) {
+  res.render("about");
+})
+
+app.listen(3000, function() {
+  console.log("Server is running on port 3000");
+})
